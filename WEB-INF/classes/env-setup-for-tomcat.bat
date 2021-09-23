bbce7908802d3c23115f5d86bf18e840d08931c3
Prashant Bhutani <47116944+prabhutani@users.noreply.github.com>
2021-09-23 00:40:44 -0500

    Add files via upload

---
+++ WEB-INF/classes/env-setup-for-tomcat.bat
@@ -0,0 +1,11 @@
+set JAVA_HOME=C:\Program Files\Java\jdk-16.0.1
+
+set PATH="C:\Program Files\Java\jdk-16.0.1\bin";%PATH%
+
+set CLASSPATH=.;C:\apache-tomcat-9.0.52\lib\servlet-api.jar;C:\apache-tomcat-9.0.52\lib\jsp-api.jar;C:\apache-tomcat-9.0.52\lib\el-api.jar;C:\apache-tomcat-9.0.52\lib\commons-beanutils-1.8.3.jar
+
+set ANT_HOME=C:\apache-tomcat-9.0.52
+
+set TOMCAT_HOME=C:\apache-tomcat-9.0.52
+
+set CATALINA_HOME=C:\apache-tomcat-9.0.52
